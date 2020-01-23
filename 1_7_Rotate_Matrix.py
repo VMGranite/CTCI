fe4490226242ea5bce96c6266409bc35569256e0
VMGranite <vmgranite@gmail.com>
2020-01-23 16:29:39 -0400

    Chapter 1 Arrays & Strings - Question 1.7 Rotate Matrix - Update to allow for irregular matrices

--- 1_7_Rotate_Matrix.py
+++ 1_7_Rotate_Matrix.py
@@ -6,31 +6,55 @@
 # rotate the image by 90 degrees. Can you do this in place?2
 
 def rotate_matrix(matrix):
+	isIrregular = False
+	print("\n**======Start=======**")
 	print("\nCurrent Matrix:")
 	for row in matrix:
 		print(row)
 
-	#get the dimensions of the current matrix
+	# Get the dimensions of the current matrix: 
 	numOfRows = len(matrix)
-	numOfColumns = len(matrix[0])
+	# Here we are getting the maximum column length of the 
+	# longest column incase the matrix is an irregular length
+	numOfColumns = 0
+	for row in matrix:
+		#find the longest list to find the max column length
+		columnLength = len(row)
+		if columnLength > numOfColumns:
+			numOfColumns = columnLength
+
+	# If the matrix is an irregular shape, we will need to fill in the missing spots
+	# Otherwise we cannot rotate the matrix
+	for row in matrix:
+		if len(row) < numOfColumns:
+			while len(row) < numOfColumns:
+				row.append(None)
+				isIrregular = True
+
+
+	if isIrregular:
+		print("\nUpdated IRREGULAR Current Matrix:")
+		for row in matrix:
+			print(row)
+
 	print("\nNumber of Rows: " + str(numOfRows))
 	print("Number of Columns: " + str(numOfColumns) +"\n")
 	print("-----------------\n")
 
-	#create new matrix
+	# Create new matrix
 	print("Empty Rotated Matrix:")
-	#For more information on 2D Matrices in Python
-	#https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
-	rotatedMatrix = [[0 for i in range(numOfRows)] for j in range(numOfColumns)]
+	# For more information on 2D Matrices in Python
+	# https://www.geeksforgeeks.org/python-using-2d-arrays-lists-the-right-way/
+	rotatedMatrix = [[None for i in range(numOfRows)] for j in range(numOfColumns)]
 	for row in rotatedMatrix: 
    		print(row) 
 	print("\nNumber of Rows: " + str(numOfColumns))
 	print("Number of Columns: " + str(numOfRows) + "\n")
 
    	
-	#iterate through the current matrix and update the new rotated matrix
-	#The Rotated Matrix's ROW will be the Current Matrix's COLUMN
-	#The Rotated Matrix's Column will be the Current Matrix's ROW, but iterated backwards
+	# Iterate through the current matrix and update the new rotated matrix
+	# The Rotated Matrix's ROW will be the Current Matrix's COLUMN
+	# The Rotated Matrix's Column will be the Current Matrix's ROW, but iterated backwards
 	print("#########################")
 
 	rowOffset = numOfRows
@@ -50,7 +74,7 @@ def rotate_matrix(matrix):
 		for column in range(0, numOfColumns):
 			print("Original Row: " + str(row))
 			print("Original Column: " + str(column))
-			print("Item: " + matrix[row][column])
+			print("Item: " + str(matrix[row][column]))
 			print("Rotated Row: " + str(column))
 			print("Rotated Column: " + str(rowOffset))
 
@@ -59,16 +83,19 @@ def rotate_matrix(matrix):
 			print(rotatedMatrix[column])
 			print("\n")
 
-			subtractOne = True
-
 		print("-------------------------")
 
 	print("#### Rotated Matrix ####")
 	for row in rotatedMatrix: 
    		print(row)
 
-#Square and Rectanglular Matrices
-#alphabetical_matrix = [['A','B','C'], ['D','E','F'], ['G','H','I']]
-#alphabetical_matrix = [['A','B','C'], ['D','E','F'], ['G','H','I'], ['J','K','L']]
-alphabetical_matrix = [['A','B','C'], ['D','E','F'], ['G','H','I'], ['J','K','L'], ['M','N','O']]
-rotate_matrix(alphabetical_matrix)
+#Test Cases
+
+#square_alphabetical_matrix = [['A','B','C'], ['D','E','F'], ['G','H','I']]
+#rotate_matrix(square_alphabetical_matrix)
+#rectangular_alphabetical_matrix = [['A','B','C'], ['D','E','F'], ['G','H','I'], ['J','K','L']]
+#rectangular_alphabetical_matrix = [['A','B','C'], ['D','E','F'], ['G','H','I'], ['J','K','L'], ['M','N','O']]
+#rotate_matrix(rectangular_alphabetical_matrix)
+
+irregular_matrix = [['A','B'], ['C','D','E','F'], ['G','H','I'], ['J','K'], ['L','M','N','O']]
+rotate_matrix(irregular_matrix)
